51269af1fa2a635d969e9b9afe818f38dff1333c
KlaudiaUrbanska <klaudiaurbanska@onet.eu>
2020-01-24 00:38:16 +0100

    Sabre Task

---
+++ src/Metods.java
@@ -0,0 +1,33 @@
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+class Metods {
+    static void charsList(String string, LinkedList<Character> list) {
+        for(int i=0;i<string.length();i++){
+            if((string.charAt(i) != ' ') && (string.charAt(i) != ',') && !list.contains(string.charAt(i))) {
+                list.add(string.charAt(i));
+            }
+        }
+    }
+
+
+    static void getResults(LinkedList<Character> characters, LinkedList<String> words, String[] wordss) {
+        for(int j=0; j<characters.size(); j++){
+            System.out.print(characters.get(j)+":");
+            for (String word : words) {
+                if (word.contains(wordss[j])) {
+                    System.out.print(word + ",");
+                }
+            }
+            System.out.println("\b");
+        }
+    }
+
+    static <T> void removeDuplicate(LinkedList<T> list)
+    {
+        Set<T> set = new LinkedHashSet<>(list);
+        list.clear();
+        list.addAll(set);
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        //String string = "ala ma kota, kot koduje w Javie Kota";
+        Scanner scanner=new Scanner(System.in);
+        String string = scanner.nextLine();
+        String stringLow = string.toLowerCase();
+        LinkedList<Character> characters = new LinkedList<>();
+        String[] seperatWords = stringLow.split("[, .!?]");
+        LinkedList<String> words = new LinkedList<>(Arrays.asList(seperatWords));
+
+        Metods.charsList(stringLow,characters);
+
+        Collections.sort(characters);
+
+        Collections.sort(words);
+
+        Metods.removeDuplicate(words);
+
+        String[] wordss= new String[characters.size()];
+
+        for(int h=0; h<characters.size();h++){
+            wordss[h]=characters.get(h).toString();
+        }
+
+        Metods.getResults(characters, words, wordss );
+    }
+
+}
+
